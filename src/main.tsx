27c59aeec40b677ddc95877cfa8d91e7a6beb85d
Phil <103880143+Sharppegs@users.noreply.github.com>
2023-06-15 18:46:57 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import {ContextProvider} from "./Context.jsx"
+import {ContextProvider} from "./Context"
 import App from './App.tsx'
 import './index.css'
 
