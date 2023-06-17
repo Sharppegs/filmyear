31a6cf00f4a6f1cd84ea35b272376857b4936dd9
Phil <103880143+Sharppegs@users.noreply.github.com>
2023-06-17 14:51:18 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import {ContextProvider} from "./Context.jsx"
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
